b64f1ca5270944653d31fcf6ece506bcceeaaea4
Jesper Gulmann Henriksen <jesper@gulmann.dk>
2022-10-06 21:02:58 +0200

    Commit of Episode 01

---
+++ 1/Examples/1 - Dynamic as a Better Pattern Matching/Program.cs
@@ -0,0 +1,16 @@
+﻿using DotNetCoach.Episode1;
+
+List<object> mixOfObjects = new()
+{
+    true,
+    87,
+    "Hello World",
+    176.0,
+    new {X = 42, Y = 87}
+};
+
+foreach (dynamic o in mixOfObjects)
+{
+    // Perform distinct handling depending upon the runtime type of o
+    ObjectHandler.Handle(o); // <-- runtime decides!
+}
---
+++ 1/README.md
@@ -0,0 +1,3 @@
+# Episode 1 - CS Tricks: Dynamic as a Better Pattern Matching
+
+Accompanying code for [YouTube Video](https://www.youtube.com/watch?v=yFZwXtUGuzk)
